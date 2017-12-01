ed9fc2319c43cac2fc14accf8733025e5d78f9ac
Sparksjj <e.gursky@mobiles.by>
2017-12-01 16:39:32 +0300

    https => http

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   googleApi: '',
   api: 'https://api.iqeon.io/api/v1/',
-  socketUrl: 'https://api.iqeon.io',
+  socketUrl: 'http://api.iqeon.io',
   socketPort: ':6001'
 };
--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   googleApi: '',
   api: 'https://iqeon.stage.farm/api/v1/',
-  socketUrl: 'https://iqeon.stage.farm',
+  socketUrl: 'http://iqeon.stage.farm',
   socketPort: ':6001'
 };
