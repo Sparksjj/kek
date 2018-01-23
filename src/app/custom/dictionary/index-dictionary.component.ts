95d9172bfd1418d50261ff4c087d2d75220917ca
Ruslan Tursunov <r.tursunov@naveksoft.com>
2018-01-23 19:27:06 +0300

    Update dropdown for new dictionaries

--- src/app/custom/dictionary/index-dictionary.component.ts
+++ src/app/custom/dictionary/index-dictionary.component.ts
@@ -1,11 +1,5 @@
 import { ActivatedRoute, Router } from '@angular/router';
-import {
-  AfterViewInit,
-  Component,
-  OnDestroy,
-  OnInit,
-  ViewChild
-} from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 
 import { AppMemoryService } from '../../core/app-memory.service';
 import { ApplicationHttpClient } from '../../core/http-client';
@@ -14,7 +8,7 @@ import JSONEditor from 'jsoneditor';
 @Component({
   selector: 'ms-dictionary',
   templateUrl: './index-dictionary.component.html',
-  styleUrls: ['./index-dictionary.component.scss']
+  styleUrls: ['./index-dictionary.component.scss'],
 })
 export class IndexDictionaryComponent implements OnInit, OnDestroy {
   @ViewChild('translateInput') translateInput;
@@ -27,7 +21,7 @@ export class IndexDictionaryComponent implements OnInit, OnDestroy {
   public fileErr: any;
   public fileInvalid: any;
   public load = false;
-  public langs = ['ru', 'en', 'cn', 'es', 'vn', 'kp'];
+  public langs = [];
 
   constructor(
     public http: ApplicationHttpClient,
@@ -51,11 +45,7 @@ export class IndexDictionaryComponent implements OnInit, OnDestroy {
 
     const container = document.getElementById('jsoneditor');
     const options = { mode: 'form' };
-    this.editor = new JSONEditor(
-      container,
-      <any>options,
-      JSON.parse(this.parsedJSON)
-    );
+    this.editor = new JSONEditor(container, <any>options, JSON.parse(this.parsedJSON));
 
     this.editor.expandAll();
     this.editor.focus();
@@ -114,7 +104,7 @@ export class IndexDictionaryComponent implements OnInit, OnDestroy {
     this.parsedJSON = JSON.stringify(this.editor.get(), null, 2);
 
     const body = {
-      json: this.parsedJSON
+      json: this.parsedJSON,
     };
 
     console.log('BODY', body);
