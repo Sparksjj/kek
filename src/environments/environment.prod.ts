ce1e8486268c272f11364af7def3c1a7284f5f0c
Ruslan Tursunov <r.tursunov@naveksoft.com>
2018-01-25 17:21:48 +0300

    Fix weblink in env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   googleApi: '',
   api: 'https://api.iqeon.io/api/v1/',
-  vebLink: 'https://iqeon.stage.farm',
+  vebLink: 'https://iqeon.io',
   socketUrl: 'https://api.iqeon.io',
   socketPort: ':6001',
 };
