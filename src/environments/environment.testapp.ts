5ec2b6af2bb6671948c0234c0ae7e229eb6519eb
Sparksjj <e.gursky@mobiles.by>
2018-02-26 23:56:08 +0300

    add test env api.la2.farm

---
+++ src/environments/environment.testapp.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  googleApi: '',
+  api: 'http://api.la2.farm/api/v1/',
+  vebLink: 'https://iqeon.io',
+  socketUrl: 'http://api.la2.farm',
+  socketPort: ':6001',
+};
